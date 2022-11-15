973358973b866eef2a275e4a6718c8605d371692
Shivam Yadav <115492003+Shivamyadav4344@users.noreply.github.com>
2022-11-15 14:28:50 +0530

    Update itertools.py

--- itertools.py
+++ itertools.py
@@ -17,19 +17,7 @@ print(list(produ))
 prod1 = product(e,f,repeat = 2)
 print(list(prod1))
 
-from itertools  import permutations 
-a = [1,2,3]
-perm = permutations(a)
-print("Permutations=",list(perm))
 
-perm1 = permutations(a,2)
-print("Permutations 2 =",list(perm1))
-
-b = [2,4,6]
-per = permutations(b)
-print("Permutations_own1=",list(per))
-per1  = permutations(b,2)
-print("Permutations_1=",list(per1))
 
 
 
